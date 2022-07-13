bda38860414f1d1a1ddcc6600dd916501f32f767
Vitória Vicente <viviicente@outlook.com>
2022-07-13 20:25:34 -0300

    first commit

---
+++ src/comando/PrimeiroPrograma.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package comando;
+
+/**
+ * @author Vitória
+ *
+ */
+public class PrimeiroPrograma {
+	
+	public static void main(String[] args){
+
+
+	     System.out.println("Hello World");
+
+	}
+
+}
